5937c899ae23a557fb5f36c45b4fcc37a74cdde4
Aditya Thorat <thorat.aditya@outlook.com>
2022-05-02 15:49:23 +0530

    add css and links

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,27 @@
+import Head from "next/head"
+import Link from "next/link"
+import Image from "next/image"
+import Layout from "../../components/layout"
+
 export default function FirstPost() {
-    return <h1>First Post</h1>
+    return (
+        <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+
+            <Image
+                src="/images/profile.jpg"
+                height={144}
+                width={144}
+                alt="Your Name"
+            />
+        </>
+    )
 }
